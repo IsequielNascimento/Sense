932ca043c9394edef1efe5744d21f284c4953b35
IsequielNascimento <isequielnascimento@gmail.com>
2026-07-20 17:34:18 -0300

    feat: campos da tela dinamica em Etapa

--- Assets/Scripts/Data/Etapa.cs
+++ Assets/Scripts/Data/Etapa.cs
@@ -1,8 +1,16 @@
 [System.Serializable]
 public class Etapa
 {
-    public string animacao;
     public string tutorial;
+    public string animacao;
     public string telaDisplay;
     public string vfx;
+
+    public string textoDisplay;
+    public string alerta;
+    public string leds;
+    public float progressoSegundos;
+    public bool progressoEstoura;
+    public string alertaTempoExcedido;
+    public string textoAngulo;
 }
