367ff5c2674e892bef889d70b388c17640f8155d
IsequielNascimento <isequielnascimento@gmail.com>
2026-08-28 08:38:26 -0300

    feat: remove the legacy AR scene and its dead navigation

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,12 +5,6 @@ public class Menu : MonoBehaviour
 {
     public void LoadScenes(string cena)
     {
-        if (cena == Scenes.ArLegacy)
-        {
-            ControleDeCena.Instance.DefinirOrigem(OrigemCena.Montagem);
-
-        }
-
         SceneManager.LoadScene(cena);
     }
 }
--- Assets/Scripts/Scenes.cs
+++ Assets/Scripts/Scenes.cs
@@ -4,6 +4,5 @@ public static class Scenes
     public const string AssemblyWarning = "Montagem2";
     public const string Problems = "Problemas";
     public const string DigitalTwin = "CenaGemeo";
-    public const string ArLegacy = "ARMudanca";
     public const string ArUiToolkit = "AR_Cena_UIToolkit";
 }
